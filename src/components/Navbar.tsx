22ddc2ade7d44e45b7278702a7a76c503e0aab28
Mizael Rios da Silva <mizael_sti@hotmail.com>
2023-08-13 16:58:17 -0300

    feat: Add component CustomButtonProps

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const NavBar = () => (
   <header className='w-full  absolute z-10'>
@@ -13,6 +14,12 @@ const NavBar = () => (
           className='object-contain'
         />
       </Link>
+
+      <CustomButton
+        title='Entrar'
+        btnType='button'
+        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+      />
     </nav>
   </header>
 );
