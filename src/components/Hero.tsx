e0998f077d0b13ba6ecf0d6f5f6464b2ee60c392
Mizael Rios da Silva <mizael_sti@hotmail.com>
2023-08-13 19:27:39 -0300

    chore: add icon and png

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import CustomButton from "./CustomButton";
+import Image from "next/image";
 
 const Hero = () => {
   const handleScroll = () => {
@@ -29,7 +30,9 @@ const Hero = () => {
         />
       </div>
       <div className="hero__image-container">
-       
+        <div className="hero__image">
+          <Image src="/hero_sun_lightning_rain.png" fill alt="hero" className="object-contain" />
+        </div>
         <div className="hero__image-overlay" />
       </div>
     </div>
