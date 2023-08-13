396205ca6e7bd8dc5269e15103b679fbc6196ffa
Mizael Rios da Silva <mizael_sti@hotmail.com>
2023-08-13 17:38:34 -0300

    feat: add component Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image';
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <main className='overflow-hidden'>
-      
+      <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Cidades</h1>
+          <p>Pesquisa rápida</p>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Consulte, busque, veja como está o clima hoje na sua cidade!
+        </h1>
+
+        <p className="hero__subtitle">
+          De forma simples e fácil veja se vai chover ou fazer sol hoje na sua cidade.
+        </p>
+
+        <CustomButton
+          title="Consultar sua cidade"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+       
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
